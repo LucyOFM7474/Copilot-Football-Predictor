0c44e45acea30fde6cf7fd1953efbf79d1079dc1
LucyOFM7474 <osiacflorinmarian@gmail.com>
2025-08-14 22:05:47 +0300

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import ChatBox from '../components/ChatBox';
+
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>💬 FlorinBot</h1>
+      <ChatBox />
+    </main>
+  );
+}
